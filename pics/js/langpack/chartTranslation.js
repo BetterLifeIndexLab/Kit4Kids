3311024e3ee12b5f6abec03a42b8051c5520a335
BetterLifeIndexLab <bli@oecd.org>
2018-11-21 21:10:01 +0100

    Add files via upload

---
+++ pics/js/langpack/chartTranslation.js
@@ -0,0 +1,9 @@
+function label2update(lang){
+	console.log(lang)
+  if(lang=="fr")
+  	$(".kitdownload").attr("href", "kit/Kit4Kid_FR.zip")
+  else if(lang=="en")
+  	$(".kitdownload").attr("href", "kit/Kit4Kid_EN.zip")
+
+}
+
